e4174d2b6c06a8097a126e57616700098b9ee03b
LINE WORKS <57744269+lineworks-test@users.noreply.github.com>
2019-11-14 23:53:35 +0900

    Add files via upload

---
+++ conf/config.py
@@ -0,0 +1,31 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+# account
+LANG = "kr"    # ["kr"|"en"|"jp"]
+ADMIN_ACCOUNT = "admin@lwbottest02"
+DOMAIN_ID = 10123847
+
+# api
+API_ID = "jp1HOduOeHyIK"
+CONSUMER_KEY = "4d66rICCBuJtLIo5XYgf"
+
+TOKEN = None
+SERVER_ID = "b66941c7d70a4e409c610bb22c316995"
+PRIVATE_KEY_NAME = "private_20191114214854key"
+
+# api 정보는 아직 작성 완료 하지 못한
+
+# LOCAL ADDRESS
+LOCAL_ADDRESS = "https:lineworks-sample-bot.herokuapp.com"
+
+# DB config
+DB_HOST = "ec2-54-83-192-245.compute-1.amazonaws.com
+DB_PORT = "5432"
+DB_NAME = "d5fmafk0444b7f"
+DB_USER = "cqbggjvngqijwy"
+DB_PASSWORD = "8aabb3e3f1b63abf8ebdbe58ec6f0237910e16aefcfa8fc77978fbd6bf1e7ed9"
+DB_SSLMODE = "require"
+
+# db adress 및 db config는 전부 변경 완료 함
